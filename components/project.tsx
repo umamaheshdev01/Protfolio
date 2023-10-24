e0296bf72b1d3ef283076d88896d9b19c149f6e5
umamaheshdev01 <139320517+umamaheshdev01@users.noreply.github.com>
2023-10-25 01:46:26 +0530

    Ok

--- components/project.tsx
+++ components/project.tsx
@@ -12,6 +12,7 @@ export default function Project({
   description,
   tags,
   imageUrl,
+  link
 }: ProjectProps) {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
